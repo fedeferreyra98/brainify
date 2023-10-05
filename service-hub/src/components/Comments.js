5ce09954b44746b49e4a74b9cdc8eff096a37179
baufantauzzo <bautistafantauzzo@gmail.com>
2023-10-05 10:54:07 -0300

    Separe las funciones de los comentarios Agregue el archivo notification para manejar usarlo de plantilla Agregue el archivo mockComments con los comentarios de ejemplo

--- service-hub/src/components/Comments.js
+++ service-hub/src/components/Comments.js
@@ -12,109 +12,79 @@ import {
   CardContent,
   CardActions,
   Rating,
-  Snackbar,
 } from '@mui/material';
+import Notification from './Notification';
+import mockComments from './mockComments';
+
+function CommentActions({ onPublish, onDelete }) {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={6}>
+        <Button
+          variant="contained"
+          color="primary"
+          fullWidth
+          onClick={onPublish}
+        >
+          Publicar
+        </Button>
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Button
+          variant="contained"
+          color="secondary"
+          fullWidth
+          onClick={onDelete}
+        >
+          Borrar
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
+
+function CommentCard({ comment, onPublish, onDelete }) {
+  return (
+    <Card sx={{ width: '100%', mb: 2 }}>
+      <CardContent>
+        <ListItemText
+          primary={comment.user}
+          secondary={
+            <>
+              <Typography
+                component="span"
+                variant="body2"
+                color="textPrimary"
+                sx={{ display: 'block', fontWeight: 'bold' }}
+              >
+                {comment.serviceName}
+              </Typography>
+              <Rating value={comment.rating} readOnly />
+              <Typography
+                component="span"
+                variant="body2"
+                color="textPrimary"
+                sx={{ display: 'block' }}
+              >
+                {comment.comment}
+              </Typography>
+              {comment.secondaryComment}
+            </>
+          }
+        />
+      </CardContent>
+      <CardActions>
+        <CommentActions
+          onPublish={() => onPublish(comment.id)}
+          onDelete={() => onDelete(comment.id)}
+        />
+      </CardActions>
+    </Card>
+  );
+}
 
 function Comments() {
-  const [comments, setComments] = useState([
-    {
-      id: 1,
-      user: 'Alejandro García Pérez',
-      serviceName: 'Conferencia de Historia Moderna',
-      comment: '¡Impresionante ponencia!',
-      secondaryComment: 'La explicación del profesor fue clara y concisa.',
-      rating: 5,
-    },
-    {
-      id: 2,
-      user: 'Emma Smith Johnson',
-      serviceName: 'Taller de Ciencias Naturales',
-      comment: 'Muy enriquecedor.',
-      secondaryComment: 'Aprendí mucho en la charla de hoy.',
-      rating: 4,
-    },
-    {
-      id: 3,
-      user: 'Maria Rossi Bianchi',
-      serviceName: 'Seminario de Matemáticas Avanzadas',
-      comment: '¡Gran contenido!',
-      secondaryComment: 'La lección sobre historia fue fascinante.',
-      rating: 5,
-    },
-    {
-      id: 4,
-      user: 'Ahmed Ali Khan',
-      serviceName: 'Curso de Literatura Clásica',
-      comment: '¡Bien hecho!',
-      secondaryComment: 'La metodología de enseñanza es única.',
-      rating: 4,
-    },
-    {
-      id: 5,
-      user: 'Sofia Rodriguez Fernandez',
-      serviceName: 'Clase de Física Cuántica',
-      comment: 'Muy informativo.',
-      secondaryComment: 'La clase de ciencias superó mis expectativas.',
-      rating: 3,
-    },
-    {
-      id: 6,
-      user: 'Olga Ivanova Petrovna',
-      serviceName: 'Webinar de Tecnologías Emergentes',
-      comment: 'Excelente interacción.',
-      secondaryComment: 'Me gustó cómo el profesor involucró a la audiencia.',
-      rating: 2,
-    },
-    {
-      id: 7,
-      user: 'Olga Ivanova Petrovna',
-      serviceName: 'Taller de Escritura Creativa',
-      comment: 'Contenido relevante.',
-      secondaryComment: 'La lección sobre tecnología fue muy actual.',
-      rating: 3,
-    },
-    {
-      id: 8,
-      user: 'Mia Nguyen Tran',
-      serviceName: 'Conferencia de Filosofía Contemporánea',
-      comment: 'Muy bien estructurado.',
-      secondaryComment: 'La clase estuvo organizada y fluyó sin problemas.',
-      rating: 4,
-    },
-    {
-      id: 9,
-      user: 'Luca Costa Silva',
-      serviceName: 'Seminario de Psicología Positiva',
-      comment: '¡Inspirador!',
-      secondaryComment: 'La charla de motivación realmente me tocó.',
-      rating: 5,
-    },
-    {
-      id: 10,
-      user: 'Priya Patel Sharma',
-      serviceName: 'Curso de Biología Marina',
-      comment: 'Gran uso de recursos.',
-      secondaryComment: 'Las diapositivas y videos fueron muy útiles.',
-      rating: 5,
-    },
-    {
-      id: 11,
-      user: 'Ivan Petrovich Sokolov',
-      serviceName: 'Taller de Fotografía Digital',
-      comment: 'Clase práctica.',
-      secondaryComment: 'Me gustó la forma en que se llevó a cabo el taller.',
-      rating: 4,
-    },
-    {
-      id: 12,
-      user: 'Isabella Santos Ferreira',
-      serviceName: 'Conferencia de Diseño Gráfico',
-      comment: '¡Muy recomendable!',
-      secondaryComment:
-        'Definitivamente asistiré a más clases de este profesor.',
-      rating: 3,
-    },
-  ]);
+  const [comments, setComments] = useState(mockComments);
 
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
@@ -151,72 +121,19 @@ function Comments() {
         <List>
           {comments.map((comment) => (
             <ListItem key={comment.id} disablePadding>
-              <Card sx={{ width: '100%', mb: 2 }}>
-                <CardContent>
-                  <ListItemText
-                    primary={comment.user}
-                    secondary={
-                      <>
-                        <Typography
-                          component="span"
-                          variant="body2"
-                          color="textPrimary"
-                          sx={{ display: 'block', fontWeight: 'bold' }}
-                        >
-                          {comment.serviceName}
-                        </Typography>
-                        <Rating value={comment.rating} readOnly />
-                        <Typography
-                          component="span"
-                          variant="body2"
-                          color="textPrimary"
-                          sx={{ display: 'block' }}
-                        >
-                          {comment.comment}
-                        </Typography>
-                        {comment.secondaryComment}
-                      </>
-                    }
-                  />
-                </CardContent>
-                <CardActions>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12} md={6}>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        fullWidth
-                        onClick={() => handlePublish(comment.id)}
-                      >
-                        Publicar
-                      </Button>
-                    </Grid>
-                    <Grid item xs={12} md={6}>
-                      <Button
-                        variant="contained"
-                        color="secondary"
-                        fullWidth
-                        onClick={() => handleDelete(comment.id)}
-                      >
-                        Borrar
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </CardActions>
-              </Card>
+              <CommentCard
+                comment={comment}
+                onPublish={handlePublish}
+                onDelete={handleDelete}
+              />
             </ListItem>
           ))}
         </List>
       </Box>
-      <Snackbar
+      <Notification
         open={openSnackbar}
-        autoHideDuration={1500}
-        onClose={handleCloseSnackbar}
         message={snackbarMessage}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-        ContentProps={{
-          style: { backgroundColor: 'green', color: 'white' },
-        }}
+        onClose={handleCloseSnackbar}
       />
     </Container>
   );
