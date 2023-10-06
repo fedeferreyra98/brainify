a5f8e2dcc37383e3728d29412f7078be5cd115ef
baufantauzzo <bautistafantauzzo@gmail.com>
2023-10-06 08:52:31 -0300

    Puse brainify en la barra de arriba cuando se achica, antes decia logo

--- service-hub/src/context/NavBar.jsx
+++ service-hub/src/context/NavBar.jsx
@@ -144,7 +144,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            Brainify
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
