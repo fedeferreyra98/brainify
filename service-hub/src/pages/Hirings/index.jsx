a4840279fdb6cea3be67628805c13430a80be910
baufantauzzo <bautistafantauzzo@gmail.com>
2023-10-06 15:48:34 -0300

    contrataciones en tarjetas y texto en blanco al pasar el mouse

--- service-hub/src/pages/Hirings/index.jsx
+++ service-hub/src/pages/Hirings/index.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
-import { Container, Typography, Tabs, Tab } from '@mui/material';
+import {
+  Container,
+  Typography,
+  Tabs,
+  Tab,
+  Pagination,
+  Box,
+} from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import HiringList from './HiringList';
+import mockHirings from '../../data/mockHirings';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,33 +20,23 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     backgroundColor: theme.palette.background.paper,
   },
+  tabHover: {
+    '&:hover': {
+      color: '#ffffff',
+    },
+  },
 }));
 
 function Hirings() {
   const classes = useStyles();
 
-  // Estado inicial para las contrataciones
-  const [contrataciones, setContrataciones] = useState([
-    {
-      id: 1,
-      servicio: 'Clase de Matemáticas',
-      usuario: 'Juan Pérez',
-      numeroTelefonico: '1544054552',
-      horarioContacto: '10:30-18:00',
-      estado: 'solicitada',
-    },
-    {
-      id: 2,
-      servicio: 'Clase de Aleman',
-      usuario: 'Benjamin Fernandez',
-      numeroTelefonico: '1148766551',
-      horarioContacto: '08:00-20:00',
-      estado: 'aceptada',
-    },
-    // ... puedes agregar más contrataciones iniciales si lo deseas
-  ]);
+  const [contrataciones, setContrataciones] = useState(mockHirings);
+  const [currentTab, setCurrentTab] = useState('General');
 
-  // Función para cambiar el estado de una contratación
+  const handleChangeTab = (event, newValue) => {
+    setCurrentTab(newValue);
+  };
+
   const handleEstadoChange = (id, nuevoEstado) => {
     const updatedContrataciones = contrataciones.map((contratacion) => {
       if (contratacion.id === id) {
@@ -48,11 +46,18 @@ function Hirings() {
     });
     setContrataciones(updatedContrataciones);
   };
-  const [currentTab, setCurrentTab] = useState('General');
 
-  const handleChangeTab = (event, newValue) => {
-    setCurrentTab(newValue);
-  };
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
+  // Filtrar contrataciones basado en la pestaña actual
+
+  const filteredContrataciones =
+    currentTab === 'General'
+      ? contrataciones
+      : contrataciones.filter((c) => c.estado === currentTab);
+
+  // Calcular el número total de páginas basado en las contrataciones filtradas
+  const totalPages = Math.ceil(filteredContrataciones.length / itemsPerPage);
 
   return (
     <div>
@@ -69,21 +74,38 @@ function Hirings() {
           centered
           className={classes.tabs}
         >
-          <Tab label="General" value="General" />
-          <Tab label="Solicitada" value="solicitada" />
-          <Tab label="Aceptada" value="aceptada" />
-          <Tab label="Finalizada" value="finalizada" />
+          <Tab label="General" value="General" className={classes.tabHover} />
+          <Tab
+            label="Solicitada"
+            value="solicitada"
+            className={classes.tabHover}
+          />
+          <Tab label="Aceptada" value="aceptada" className={classes.tabHover} />
+          <Tab
+            label="Finalizada"
+            value="finalizada"
+            className={classes.tabHover}
+          />
         </Tabs>
 
         <HiringList
-          contrataciones={
-            currentTab === 'General'
-              ? contrataciones
-              : contrataciones.filter((c) => c.estado === currentTab)
-          }
+          contrataciones={filteredContrataciones.slice(
+            (currentPage - 1) * itemsPerPage,
+            currentPage * itemsPerPage
+          )}
           handleEstadoChange={handleEstadoChange}
           classes={classes}
         />
+
+        {totalPages > 1 && (
+          <Box display="flex" justifyContent="center" marginTop="20px">
+            <Pagination
+              count={totalPages}
+              page={currentPage}
+              onChange={(event, value) => setCurrentPage(value)}
+            />
+          </Box>
+        )}
       </Container>
     </div>
   );
