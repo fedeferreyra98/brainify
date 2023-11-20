97f1267078808539c454c35acf0d0a32565c8d21
Federico <feferreyra98@gmail.com>
2023-11-20 18:21:30 -0300

    implements service repository

---
+++ backend/repositories/service.repository.js
@@ -0,0 +1,56 @@
+import {Service} from "../models/service.model.js";
+
+class ServiceRepository{
+
+    async getAll(category){
+        const query = category === "all" ? {} : {category};
+        return await Service.find(query).lean();
+    };
+
+    async getByUserId(userId){
+        return await Service.find({userId}).lean();
+    };
+
+    async create(serviceData){;
+        return await service.create(serviceData);
+    }
+
+    async getById(id){
+        return await Service.findById(id).lean();
+    }
+
+    async update(id, userId, update){
+        const service = await findServiceAndCheckOwnership(id, userId);
+        Object.keys(update).forEach((key) => (service[key] = update[key]));
+        await service.save();
+        return service;
+    };
+    
+    async remove(id, userId){
+        const service = await findServiceAndCheckOwnership(id, userId);
+        await service.deleteOne();
+    };
+
+    async validateCategory(category){
+        return category === "all" ||  checkCategory(category);
+    }
+}
+
+    const findServiceAndCheckOwnership= async (serviceId, userId) => {
+    const service = await Service.findById(serviceId);
+    if (!service) {
+      throw new Error("Servicio no encontrado");
+    }
+  
+    if (service.userId.toString() !== userId) {
+      throw new Error("El servicio no pertenece al usuario");
+    }
+  
+    return service;
+  };
+
+    const checkCategory = (category) => {
+    return ["programacion", "idiomas", "musica", "matematica"].includes(
+      category
+    );
+  };
