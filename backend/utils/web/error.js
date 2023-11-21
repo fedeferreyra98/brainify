d9aeee255469fc4173975dc648cae89487883d6c
Federico <feferreyra98@gmail.com>
2023-11-21 14:15:53 -0300

    Adds web utilities to centralize error handling in response

---
+++ backend/utils/web/error.js
@@ -0,0 +1,15 @@
+export const handleError = (response, error) => {
+    console.log(error);
+    if (error.message === "Service not found") {
+        return response.status(404).json({message: error.message});
+        }
+    else if (error.message === "Service does not belong to user") {
+        return response.status(403).json({message: error.message});
+        }
+    else if (error.kind === "ObjectId") {
+        return response.status(400).json({message: "Id Format not valid"});
+        }
+    else {
+        return response.status(500).json({message: "Internal Server Error"});
+    }
+}
