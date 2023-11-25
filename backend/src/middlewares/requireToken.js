28bdc55cdea7f530302c91111499e62a494afcba
Federico Ferreyra <federico.ferreyra@mercadolibre.com>
2023-11-25 00:15:25 -0300

    implements requireToken middleware

---
+++ backend/src/middlewares/requireToken.js
@@ -0,0 +1,21 @@
+import * as jwt from "jsonwebtoken";
+
+export const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      throw new Error("No se proveyó el token");
+    }
+    const  userId  = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      errors: [
+        {
+          message: error.message,
+        },
+      ],
+    });
+  }
+};
