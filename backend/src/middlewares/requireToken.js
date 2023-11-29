1834fd844018d6bda8d3baa656cb18ecf1002e45
Federico Ferreyra <federico.ferreyra@mercadolibre.com>
2023-11-29 11:59:46 -0300

    Refactor: refactors verifytoken middleware function

--- backend/src/middlewares/requireToken.js
+++ backend/src/middlewares/requireToken.js
@@ -1,4 +1,5 @@
-import * as jwt from "jsonwebtoken";
+// @ts-ignore
+import jwt from "jsonwebtoken";
 
 export const requireToken = async (req, res, next) => {
   try {
@@ -6,10 +7,15 @@ export const requireToken = async (req, res, next) => {
     if (!token) {
       throw new Error("No se proveyó el token");
     }
-    const  userId  = jwt.verify(token, 'mySuperSecretKey12345!');
-    req.userId = userId;
-    next();
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+          throw err;  // Or handle the error as you see fit
+      }
+      req.userId = decoded.id;  // Assuming your payload contains the user ID as 'id'
+      next();
+    });
   } catch (error) {
+    console.log(error);
     return res.status(401).json({
       errors: [
         {
