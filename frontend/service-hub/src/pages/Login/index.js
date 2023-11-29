9303138e84f74eebb4075f3546d952a940d6efc3
baufantauzzo <bautistafantauzzo@gmail.com>
2023-11-29 15:18:13 -0300

    hace el login con el back y manejo de errores

--- frontend/service-hub/src/pages/Login/index.js
+++ frontend/service-hub/src/pages/Login/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container, Typography, Grid, TextField } from '@mui/material';
-import { Link } from 'react-router-dom'; // Importa el Link de react-router-dom
 import makeStyles from '@mui/styles/makeStyles';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import SimplePasswordField from '../../components/form/SimplePasswordField';
+import NotificationRed from '../../components/ui/NotificationRed';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,11 +26,47 @@ const useStyles = makeStyles((theme) => ({
 
 function LoginPage({ onLogin }) {
   const classes = useStyles();
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: '',
+  });
+  const [notificationRedOpen, setNotificationRedOpen] = useState(false);
+  const [notificationRedMessage, setNotificationRedMessage] = useState('');
 
-  const handleLoginClick = () => {
+  /*   const handleLoginClick = () => {
     // Aquí puedes agregar la lógica de autenticación si la tienes.
     // Por ahora, simplemente llamaremos a onLogin para cambiar el estado.
     onLogin();
+  }; */
+
+  // Maneja el click para iniciar sesión
+  const handleLoginClick = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        'http://localhost:4000/api/auth/login',
+        credentials
+      );
+      console.log(response.data);
+      // Aquí puedes manejar cómo almacenar el token de autenticación, por ejemplo en localStorage
+      // localStorage.setItem('token', response.data.token);
+      navigate('/'); // Redirigir a la página de inicio o donde quieras después del login
+      onLogin();
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error.response || error);
+      setNotificationRedMessage(error.response.data.errors[0].message);
+      setNotificationRedOpen(true);
+    }
+  };
+
+  // Actualiza el estado de las credenciales
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [name]: value,
+    }));
   };
 
   return (
@@ -36,32 +74,36 @@ function LoginPage({ onLogin }) {
       <Typography variant="h4" gutterBottom>
         Iniciar Sesión
       </Typography>
-      <Grid container spacing={2} justifyContent="center">
-        <Grid item xs={12}>
-          <TextField
-            className={classes.textField}
-            label="Correo electrónico"
-            variant="outlined"
-          />
+      <form onSubmit={handleLoginClick}>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={12}>
+            <TextField
+              className={classes.textField}
+              label="Correo electrónico"
+              variant="outlined"
+              name="email"
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <SimplePasswordField
+              className={classes.textField}
+              label="Contraseña"
+              variant="outlined"
+              name="password"
+              onChange={handleChange}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <SimplePasswordField
-            className={classes.textField}
-            label="Contraseña"
-            variant="outlined"
-          />
-        </Grid>
-      </Grid>
-      <Button
-        variant="contained"
-        color="primary"
-        className={classes.button}
-        onClick={handleLoginClick}
-        component={Link}
-        to="/"
-      >
-        Iniciar Sesión
-      </Button>
+        <Button
+          variant="submit"
+          color="primary"
+          className={classes.button}
+          onClick={handleLoginClick}
+        >
+          Iniciar Sesión
+        </Button>
+      </form>
       <Button
         className={classes.button}
         color="primary"
@@ -73,6 +115,11 @@ function LoginPage({ onLogin }) {
       <Typography variant="body1" className={classes.button}>
         ¿Todavía no tienes una cuenta? <Link to="/registro">Regístrate</Link>
       </Typography>
+      <NotificationRed
+        open={notificationRedOpen}
+        message={notificationRedMessage}
+        onClose={() => setNotificationRedOpen(false)}
+      />
     </Container>
   );
 }
