1887807f1939846b76812ecb8a8307ebeb669575
Federico <feferreyra98@gmail.com>
2023-11-29 14:16:56 -0300

    Install axios to integrate back and front services

---
+++ frontend/service-hub/src/api/axiosConfig.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:4000/api',
+    timeout: 3000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+    async(config) => {
+        const jwt = JSON.parse(localStorage.getItem('jwt'));
+        config.headers = {
+            ...config.headers,
+            Authorization: jwt ? `Bearer ${jwt.token}` : '',
+        };
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
