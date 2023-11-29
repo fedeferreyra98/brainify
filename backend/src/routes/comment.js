32c875d0d27a61f1580552a1c41089e68d5b0f7e
Federico <feferreyra98@gmail.com>
2023-11-29 15:45:32 -0300

    Updates comments endpoints

--- backend/src/routes/comment.js
+++ backend/src/routes/comment.js
@@ -1,11 +1,14 @@
 // @ts-ignore
 import express from "express";
-import {getCommentsByServiceId, create, update} from "../controllers/comment.controller.js";
+import {getCommentsByServiceId, getAllCommentsByServiceId, create, update} from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
 // GetByServiceId
-router.get("/", getCommentsByServiceId);
+router.get("/:serviceId", getCommentsByServiceId);
+
+// GetAllCommentsByServiceId
+router.get("/:serviceId/all", getAllCommentsByServiceId);
 
 // Create
 router.post("/", create);
