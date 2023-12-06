0b8af12e18b2ea1a1428a58b61f86d50565368aa
Federico <feferreyra98@gmail.com>
2023-12-06 20:32:23 -0300

    Implement middleware in hirings endpoints

--- backend/src/routes/hiring.js
+++ backend/src/routes/hiring.js
@@ -1,21 +1,15 @@
 // @ts-ignore
 import express from "express";
 import {getByServiceId, getByUserId, create, updateHiring, attachServiceNameToHiring } from "../controllers/hiring.controller.js";
+import { createHiringBodyValidator, updateHiringBodyValidator } from "../middlewares/validationResultExpress.js";
+import { requireToken } from "../middlewares/requireToken.js";
 
 const router = express.Router();
 
-// GetByServiceId
 router.get("/:serviceId", getByServiceId);
-
-// GetByUserId
 router.get("/:userId", getByUserId);
-
-// Create
-router.post("/:serviceId", create);
-
-// Update
-router.put("/:hiringId", updateHiring);
-
+router.post("/:serviceId", createHiringBodyValidator, create);
+router.patch("/:hiringId", requireToken, updateHiringBodyValidator, updateHiring);
 //AttachServiceNameToHiring todo: Buscar una mejor forma de hacer esto
 router.post("/attachServiceNameToHiring", attachServiceNameToHiring);
 
