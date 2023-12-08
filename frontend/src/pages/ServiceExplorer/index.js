07cfd4b3b13f3bc97cb602dc5cd0fc950da30c59
Fefe <fedeferreyra@uade.edu.ar>
2023-12-08 17:01:57 -0300

    Feat/hirings2 (#13)
    * Cambie el enum de contact time en hiring.model
    
    * volvi a hacer funcionar contratar en lo que era main
    
    * ya funcionan las contrataciones, paso a la visualizacion por parte del docente
    
    * toque un poco el back por los comentarios, anda todo pero tira error al chequear la categoria (no se donde la chequea ni para que)
    
    ---------
    
    Co-authored-by: baufantauzzo <bautistafantauzzo@gmail.com>

--- frontend/src/pages/ServiceExplorer/index.js
+++ frontend/src/pages/ServiceExplorer/index.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Container, Typography, Grid, Pagination } from '@mui/material';
 import DynamicSelect from '../../components/form/DynamicSelect';
+// eslint-disable-next-line import/no-named-as-default
 import ServiceCard from './ServiceCard';
 import ServiceDetails from './ServiceDetails';
 import useStyles from '../../styles/styles';
-import NotificationRed from '../../components/ui/NotificationRed';
-import NotificationGreen from '../../components/ui/NotificationGreen';
-import ContratacionForm from './ContratacionForm';
 import { apiGetServices } from '../../api/apiService';
 
 function ServiceExplorer() {
@@ -21,15 +19,6 @@ function ServiceExplorer() {
   const [categoryOptions, setCategoryOptions] = useState([]);
   const [tipoFiltro, setTipoFiltro] = useState('');
   const [frecuenciaFiltro, setFrecuenciaFiltro] = useState('');
-
-  // Estado para controlar el diálogo de contratación
-  const [dialogOpen, setDialogOpen] = useState(false);
-
-  // Estados para los campos del formulario de contratación
-  const [telefono, setTelefono] = useState('');
-  const [email, setEmail] = useState('');
-  const [horario, setHorario] = useState('');
-  const [mensaje, setMensaje] = useState('');
 
   const [selectedService, setSelectedService] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
@@ -72,83 +61,6 @@ function ServiceExplorer() {
     }));
     setCategoryOptions(formattedOptions);
   }, [services]);
-
-  const handleHire = () => {
-    setDialogOpen(true);
-  };
-
-  // Estados para controlar las notificaciones
-  const [notificationRedOpen, setNotificationRedOpen] = useState(false);
-  const [notificationRedMessage, setNotificationRedMessage] = useState('');
-  const [notificationGreenOpen, setNotificationGreenOpen] = useState(false);
-
-  // Función para resetear el formulario de contratación
-  const resetFormContratacion = () => {
-    setTelefono('');
-    setEmail('');
-    setHorario('');
-    setMensaje('');
-  };
-
-  // Función para verificar si todos los campos del formulario están completos
-  const isFormComplete = () => {
-    return telefono && email && horario && mensaje;
-  };
-
-  // Función para validar un número de teléfono
-  const isValidPhoneNumber = (phoneNumber) => {
-    const pattern = /^[0-9]{10}$/; // Asume un número de 10 dígitos
-    return pattern.test(phoneNumber);
-  };
-
-  // Función para validar un correo electrónico
-  const isValidEmail = () => {
-    const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    return pattern.test(email);
-  };
-
-  // Generar las opciones de horario en intervalos de 30 minutos
-  const generateTimeOptions = () => {
-    const options = [];
-    for (let i = 0; i < 24; i += 1) {
-      for (let j = 0; j < 60; j += 30) {
-        const hour = i.toString().padStart(2, '0');
-        const minute = j.toString().padStart(2, '0');
-        options.push(`${hour}:${minute}hs`);
-      }
-    }
-    return options;
-  };
-
-  // Función para validar que la hora de finalización sea mayor que la hora de inicio y otras validaciones
-  const CheckTime = () => {
-    if (!isValidPhoneNumber(telefono)) {
-      setNotificationRedMessage(
-        'Por favor, ingrese un número de teléfono válido'
-      );
-      setNotificationRedOpen(true);
-      return;
-    }
-
-    if (!isValidEmail(email)) {
-      setNotificationRedMessage(
-        'Por favor, ingrese un correo electrónico válido'
-      );
-      setNotificationRedOpen(true);
-      return;
-    }
-
-    if (!horario) {
-      setNotificationRedMessage('Se debe establecer un horario de contacto');
-      setNotificationRedOpen(true);
-      return;
-    }
-
-    // Si todas las validaciones pasan, mostrar notificación de éxito
-    setNotificationGreenOpen(true);
-    setDialogOpen(false);
-    resetFormContratacion();
-  };
 
   // Funcion de filtrado
   const filtrarServicios = () => {
@@ -238,7 +150,6 @@ function ServiceExplorer() {
                 key={servicio._id}
                 service={servicio}
                 onClick={setSelectedService}
-                onHire={handleHire}
                 validation={newComment}
                 send={setNewComment}
               />
@@ -256,7 +167,6 @@ function ServiceExplorer() {
           <ServiceDetails
             service={selectedService}
             onClose={() => setSelectedService(null)}
-            onHire={handleHire}
           />
         </Container>
         <footer className={classes.footer}>
@@ -268,32 +178,6 @@ function ServiceExplorer() {
           </Typography>
         </footer>
       </Container>
-      <ContratacionForm
-        dialogOpen={dialogOpen}
-        setDialogOpen={setDialogOpen}
-        telefono={telefono}
-        setTelefono={setTelefono}
-        email={email}
-        setEmail={setEmail}
-        horario={horario}
-        setHorario={setHorario}
-        mensaje={mensaje}
-        setMensaje={setMensaje}
-        generateTimeOptions={generateTimeOptions}
-        CheckTime={CheckTime}
-        isFormComplete={isFormComplete}
-        resetFormContratacion={resetFormContratacion}
-      />
-      <NotificationRed
-        open={notificationRedOpen}
-        message={notificationRedMessage}
-        onClose={() => setNotificationRedOpen(false)}
-      />
-      <NotificationGreen
-        open={notificationGreenOpen}
-        message="Solicitud de contacto enviada"
-        onClose={() => setNotificationGreenOpen(false)}
-      />
     </div>
   );
 }
