cfa1a4ca0517c5fd9b6cd21c693154cd79f6a173
baufantauzzo <bautistafantauzzo@gmail.com>
2023-12-09 12:35:09 -0300

    muestra los nombres de cada card y si no hay cards para mostrar lo notifica

--- frontend/src/pages/Hirings/HiringList.jsx
+++ frontend/src/pages/Hirings/HiringList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Card,
   CardContent,
@@ -8,16 +8,66 @@ import {
   FormControl,
   InputLabel,
   Select,
+  Box, // import Box for styling non-Grid message
 } from '@mui/material';
+import { apiGetServicesByUser } from '../../api/apiService';
 
 function HiringList({ contrataciones, handleEstadoChange, classes }) {
+  const [userServices, setUserServices] = useState([]);
+
+  useEffect(() => {
+    const getServiceName = async () => {
+      try {
+        if (contrataciones && contrataciones.length > 0) {
+          const response = await apiGetServicesByUser();
+
+          const serviceIdToNameMap = new Map();
+          response.forEach((service) => {
+            // eslint-disable-next-line no-underscore-dangle
+            serviceIdToNameMap.set(service._id, service.name);
+          });
+
+          const updatedContrataciones = contrataciones.map((contratacion) => ({
+            ...contratacion,
+            serviceName: serviceIdToNameMap.get(contratacion.serviceId),
+          }));
+
+          setUserServices(updatedContrataciones);
+        } else {
+          // Set an empty array if there are no contracts to prevent undefined errors.
+          setUserServices([]);
+        }
+      } catch (error) {
+        console.log('Error getting comments info:', error);
+      }
+    };
+
+    getServiceName();
+  }, [contrataciones]);
+
+  // Display a message if there are no contracts
+  if (userServices.length === 0) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="200px"
+      >
+        <Typography variant="h5">No hay contrataciones para mostrar</Typography>
+      </Box>
+    );
+  }
+
   return (
     <Grid container spacing={3}>
-      {contrataciones.map((contratacion) => (
+      {userServices.map((contratacion) => (
         <Grid item xs={12} sm={6} md={4} key={contratacion.id}>
           <Card className={classes.card}>
             <CardContent>
-              <Typography variant="h6">{contratacion.servicio}</Typography>
+              <Typography variant="h6">
+                {contratacion.serviceName || 'Loading...'}
+              </Typography>
               <Typography color="textSecondary">
                 {`E-mail: ${contratacion.contactEmail}`}
               </Typography>
