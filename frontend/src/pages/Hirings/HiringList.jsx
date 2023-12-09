a09a137361e6b4b596bc5a9ebe3ac6fb9b9d2151
Fefe <fedeferreyra@uade.edu.ar>
2023-12-09 13:43:36 -0300

    Feat/hirings2 (#14)
    * Cambie el enum de contact time en hiring.model
    
    * volvi a hacer funcionar contratar en lo que era main
    
    * ya funcionan las contrataciones, paso a la visualizacion por parte del docente
    
    * toque un poco el back por los comentarios, anda todo pero tira error al chequear la categoria (no se donde la chequea ni para que)
    
    * Arreglado el home, saque el boton de contrtatar de las cards para no complicarnos
    
    * ya visualizamos las contrataciones, paso a que se puedan aceptar, etc
    
    * ya se pueden modificar las contrataciones y se ve reflejado en front y back
    
    * muestra los nombres de cada card y si no hay cards para mostrar lo notifica
    
    ---------
    
    Co-authored-by: baufantauzzo <bautistafantauzzo@gmail.com>

--- frontend/src/pages/Hirings/HiringList.jsx
+++ frontend/src/pages/Hirings/HiringList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Card,
   CardContent,
@@ -8,27 +8,74 @@ import {
   FormControl,
   InputLabel,
   Select,
+  Box, // import Box for styling non-Grid message
 } from '@mui/material';
+import { apiGetServicesByUser } from '../../api/apiService';
 
 function HiringList({ contrataciones, handleEstadoChange, classes }) {
+  const [userServices, setUserServices] = useState([]);
+
+  useEffect(() => {
+    const getServiceName = async () => {
+      try {
+        if (contrataciones && contrataciones.length > 0) {
+          const response = await apiGetServicesByUser();
+
+          const serviceIdToNameMap = new Map();
+          response.forEach((service) => {
+            // eslint-disable-next-line no-underscore-dangle
+            serviceIdToNameMap.set(service._id, service.name);
+          });
+
+          const updatedContrataciones = contrataciones.map((contratacion) => ({
+            ...contratacion,
+            serviceName: serviceIdToNameMap.get(contratacion.serviceId),
+          }));
+
+          setUserServices(updatedContrataciones);
+        } else {
+          // Set an empty array if there are no contracts to prevent undefined errors.
+          setUserServices([]);
+        }
+      } catch (error) {
+        console.log('Error getting comments info:', error);
+      }
+    };
+
+    getServiceName();
+  }, [contrataciones]);
+
+  // Display a message if there are no contracts
+  if (userServices.length === 0) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="200px"
+      >
+        <Typography variant="h5">No hay contrataciones para mostrar</Typography>
+      </Box>
+    );
+  }
+
   return (
     <Grid container spacing={3}>
-      {contrataciones.map((contratacion) => (
+      {userServices.map((contratacion) => (
         <Grid item xs={12} sm={6} md={4} key={contratacion.id}>
           <Card className={classes.card}>
             <CardContent>
-              <Typography variant="h6">{contratacion.servicio}</Typography>
-              <Typography color="textSecondary">
-                {`Usuario: ${contratacion.usuario}`}
+              <Typography variant="h6">
+                {contratacion.serviceName || 'Loading...'}
               </Typography>
               <Typography color="textSecondary">
-                {`E-mail: ${contratacion.mail}`}
+                {`E-mail: ${contratacion.contactEmail}`}
               </Typography>
               <Typography color="textSecondary">
-                {`Tel: ${contratacion.numeroTelefonico}`}
+                {`Tel: ${contratacion.phoneNumber}`}
               </Typography>
               <Typography color="textSecondary">
-                {`Horario de contacto: ${contratacion.horarioContacto}`}
+                {`Horario de contacto: ${contratacion.preferredContactTime}`}
               </Typography>
             </CardContent>
             <FormControl
@@ -37,21 +84,22 @@ function HiringList({ contrataciones, handleEstadoChange, classes }) {
               size="small"
               style={{ marginTop: '10px' }}
             >
-              <InputLabel id={`select-label-${contratacion.id}`}>
+              <InputLabel id={`select-label-${contratacion.contractStatus}`}>
                 Estado
               </InputLabel>
               <Select
-                labelId={`select-label-${contratacion.id}`}
-                value={contratacion.estado}
+                labelId={`select-label-${contratacion.contractStatus}`}
+                value={contratacion.contractStatus}
                 onChange={(event) =>
-                  handleEstadoChange(contratacion.id, event.target.value)
+                  // eslint-disable-next-line no-underscore-dangle
+                  handleEstadoChange(contratacion._id, event.target.value)
                 }
                 label="Estado"
               >
-                <MenuItem value="solicitada">Solicitada</MenuItem>
-                <MenuItem value="aceptada">Aceptada</MenuItem>
-                <MenuItem value="cancelada">Cancelada</MenuItem>
-                <MenuItem value="finalizada">Finalizada</MenuItem>
+                <MenuItem value="Solicitada">Solicitada</MenuItem>
+                <MenuItem value="Aceptada">Aceptada</MenuItem>
+                <MenuItem value="Cancelada">Cancelada</MenuItem>
+                <MenuItem value="Finalizada">Finalizada</MenuItem>
               </Select>
             </FormControl>
           </Card>
