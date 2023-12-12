c83092ae74bd84fb7ee9e0b2843f0748dc74ec9a
Federico <feferreyra98@gmail.com>
2023-12-12 14:12:38 -0300

    Feat(EditProfile): Implements formik in update profile form

--- frontend/src/components/form/EditProfileForm.jsx
+++ frontend/src/components/form/EditProfileForm.jsx
@@ -32,78 +32,94 @@ function EditProfileForm({ isOpen, onClose, handleSubmit, initialFormData }) {
   return (
     <Dialog open={isOpen} onClose={onClose}>
       <DialogTitle>Editar Perfil</DialogTitle>
-      <DialogContent>
-        <TextField
-          margin="dense"
-          label="Nombre"
-          type="text"
-          fullWidth
-          variant="outlined"
-          value={formik.values.firstName}
-          name="firstName"
-          onChange={formik.handleChange}
-        />
-        <TextField
-          margin="dense"
-          label="Apellido"
-          type="text"
-          fullWidth
-          variant="outlined"
-          value={formik.values.lastName}
-          name="lastName"
-          onChange={formik.handleChange}
-        />
-        <TextField
-          margin="dense"
-          label="Correo Electrónico"
-          type="text"
-          fullWidth
-          variant="outlined"
-          value={formik.values.email}
-          name="mail"
-          onChange={formik.handleChange}
-        />
-        <TextField
-          margin="dense"
-          label="Nro Télefono"
-          type="text"
-          fullWidth
-          variant="outlined"
-          value={formik.values.phoneNumber}
-          name="phoneNumber"
-          onChange={formik.handleChange}
-        />
-        <TextField
-          margin="dense"
-          label="Título"
-          type="text"
-          fullWidth
-          variant="outlined"
-          value={formik.values.degree}
-          name="degree"
-          onChange={formik.handleChange}
-        />
-        <TextField
-          margin="dense"
-          label="Experiencia"
-          type="text"
-          fullWidth
-          variant="outlined"
-          multiline
-          rows={4}
-          value={formik.values.experience}
-          name="experience"
-          onChange={formik.handleChange}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose} color="primary">
-          Cancelar
-        </Button>
-        <Button onClick={formik.handleSubmit} color="primary">
-          Guardar
-        </Button>
-      </DialogActions>
+      <form onSubmit={formik.handleSubmit}>
+        <DialogContent>
+          <TextField
+            margin="dense"
+            fullWidth
+            id="firstName"
+            name="firstName"
+            label="Nombre"
+            value={formik.values.firstName}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+            helperText={formik.touched.firstName && formik.errors.firstName}
+          />
+          <TextField
+            margin="dense"
+            fullWidth
+            id="lastName"
+            name="lastName"
+            label="Apellido"
+            value={formik.values.lastName}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+            helperText={formik.touched.lastName && formik.errors.lastName}
+          />
+          <TextField
+            margin="dense"
+            fullWidth
+            id="email"
+            name="email"
+            label="Correo electrónico"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+          <TextField
+            margin="dense"
+            fullWidth
+            id="phoneNumber"
+            name="phoneNumber"
+            label="Número de teléfono"
+            value={formik.values.phoneNumber}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={
+              formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)
+            }
+            helperText={formik.touched.phoneNumber && formik.errors.phoneNumber}
+          />
+          <TextField
+            margin="dense"
+            fullWidth
+            id="degree"
+            name="degree"
+            label="Título"
+            value={formik.values.degree}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.degree && Boolean(formik.errors.degree)}
+            helperText={formik.touched.degree && formik.errors.degree}
+          />
+          <TextField
+            margin="dense"
+            fullWidth
+            id="experience"
+            name="experience"
+            label="Experiencia"
+            value={formik.values.experience}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={
+              formik.touched.experience && Boolean(formik.errors.experience)
+            }
+            helperText={formik.touched.experience && formik.errors.experience}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose} color="primary">
+            Cancelar
+          </Button>
+          <Button color="primary" variant="contained" type="submit">
+            Guardar
+          </Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 }
