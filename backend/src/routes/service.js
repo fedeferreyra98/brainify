802a3f728448715e2b6d4fd9ae1beb6c91f5316a
Federico <feferreyra98@gmail.com>
2023-12-13 21:42:45 -0300

    Feat(services): Adds Endpoint to get top 3 best rated services

--- backend/src/routes/service.js
+++ backend/src/routes/service.js
@@ -1,7 +1,7 @@
 // @ts-ignore
 import express from "express";
 
-import {GetAllServices, GetServiceById, GetServicesByUser, Create, Update, Delete} from "../controllers/service.controller.js";
+import {GetAllServices, GetServiceById, GetServicesByUser, Create, Update, Delete, GetTop3Services} from "../controllers/service.controller.js";
 import { requireToken } from "../middlewares/requireToken.js";
 import { createServiceBodyValidator } from "../middlewares/validationResultExpress.js";
 
@@ -9,6 +9,7 @@ const router = express.Router();
 
 
 router.get("/", GetAllServices);
+router.get("/top3", GetTop3Services);
 router.get("/user", requireToken, GetServicesByUser);
 router.get("/:serviceId", GetServiceById);
 router.post("/", requireToken, createServiceBodyValidator, Create);
